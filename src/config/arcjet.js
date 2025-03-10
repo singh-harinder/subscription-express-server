55f4d482ba7fa4b613602fa5584b2fdea5ed0a52
Harinder Singh <kinggouken611@gmail.com>
2025-03-10 23:54:50 +0530

    Started implementing rate limiting

---
+++ src/config/arcjet.js
@@ -0,0 +1,21 @@
+import arcjet, { shield, detectBot, tokenBucket } from '@arcjet/node';
+
+const aj = arcjet({
+  key: ARCJET_KEY,
+  characteristics: ['ip.src'],
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE'],
+    }),
+    tokenBucket({
+      mode: 'LIVE',
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
+
+export default aj;
